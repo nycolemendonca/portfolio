7a4897b75a793db7472e51d19ab7ab6d483ee24a
Nycole Mendonça <nycolemendonca4@gmail.com>
2023-05-31 20:33:33 -0300

    update projects

--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -1,6 +1,7 @@
 import './portfolio.component.css'
 
-import WeatherApp from '../../assets/img/projects/1-weather-app.png'
+import WeatherApp from '../../assets/img/projects/1-weather-app.png';
+import GithubFinder from '../../assets/img/projects/2-github-finder.png';
 
 export const Portfolio = () => {
   return (
@@ -8,6 +9,7 @@ export const Portfolio = () => {
         <h2 className='portfolioTitle'>Portfólio</h2>
 
         <div className='portfolioCards'>
+            {/* Project 01 */}
             <figure className='card'>
                 <img src={WeatherApp} alt='Weather App' className='cardImage'/>
 
@@ -28,22 +30,23 @@ export const Portfolio = () => {
                 </figcaption>
             </figure>
 
+            {/* Project 01 */}
             <figure className='card'>
-                <img src={WeatherApp} alt='Weather App' className='cardImage'/>
+                <img src={GithubFinder} alt='Weather App' className='cardImage'/>
 
-                <figcaption className='description'>
-                    <h4>Weather App</h4>
+                <figcaption className='description'> 
+                    <h4>Github Finder</h4>
                     <div className='tagsDescription'>
                         <span>HTML</span>
                         <span>CSS</span>
-                        <span>Javascript</span>
+                        <span>ReactJS</span>
                         <span>RestAPI</span>
                     </div>
-                    <p className='textDescription'>Projeto de JavaScript com OpenWeather API. O objetivo é trazer os dados climáticos de qualquer cidade do mundo, mundando a imagem de background de acordo com a cidade consultada.</p> 
+                    <p className='textDescription'>A aplicação faz uma requisição à API do Github e fornece informações atualizadas sobre o usuário. Para dar vida à aplicação, usei HTML, CSS e ReactJS. Desde o início do desenvolvimento, priorizei o design responsivo adotando a abordagem Mobile First.</p> 
 
                     <div className='descriptionButtons'>
-                        <a className='button' href='https://github.com/nycolemendonca/weather-app' target='_blank' rel='noreferrer'>Code</a>
-                        <a className='button' href='https://nycolemendonca.github.io/weather-app/'  target='_blank' rel='noreferrer'>Deploy</a>
+                        <a className='button' href='https://github.com/nycolemendonca/react-projects/tree/main/github-finder' target='_blank' rel='noreferrer'>Code</a>
+                        <a className='button' href='https://git-discover.vercel.app/'  target='_blank' rel='noreferrer'>Deploy</a>
                     </div>
                 </figcaption>
             </figure>
